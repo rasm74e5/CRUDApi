4748549ffa15dd470435d27722e4ea638b383081
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2020-10-28 12:32:43 +0100

    Add project files.

---
+++ CRUDApi/Controllers/CarsController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CRUDApi.Models;
+
+namespace CRUDApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarContext _context;
+
+        public CarsController(CarContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cars
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCar()
+        {
+            return await _context.Car.ToListAsync();
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCar(int id)
+        {
+            var car = await _context.Car.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return car;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCar([FromRoute] int id, [FromBody] Car car)
+        {
+            if (id != car.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(car).State = EntityState.Modified;
+            try { await _context.SaveChangesAsync(); }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(id))
+                {
+                    return NotFound();
+                }
+                else { throw; }
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Car>>PostCar([FromBody]Car car)
+        {
+            _context.Car.Add(car);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCar", new {id=car.Id}, car);
+        }
+        
+        // DELETE: api/Cars/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Car>> DeleteCar(int id)
+        {
+            var car = await _context.Car.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Car.Remove(car);
+            await _context.SaveChangesAsync();
+
+            return car;
+        }
+
+        private bool CarExists(int id)
+        {
+            return _context.Car.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CRUDApi/Models/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDApi.Models
+{
+    public class Car
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Colour
+        {
+            get;
+            set;
+        }
+        public double Price
+        {
+            get;
+            set;
+        }
+    }
+}
